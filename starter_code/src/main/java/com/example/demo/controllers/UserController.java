df9d6e0935612f1e80b4b9b4f25b08037efeb27e
dmullandev <dwmullan@gmail.com>
2022-11-14 23:12:54 +0000

    added JWT bearer token classes, updated user and password entities, added log4j, added spring security classes

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,33 +20,47 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private CartRepository cartRepository;
 
-	@GetMapping("/id/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		return ResponseEntity.of(userRepository.findById(id));
-	}
-	
-	@GetMapping("/{username}")
-	public ResponseEntity<User> findByUserName(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
-	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
-		User user = new User();
-		user.setUsername(createUserRequest.getUsername());
-		Cart cart = new Cart();
-		cartRepository.save(cart);
-		user.setCart(cart);
-		userRepository.save(user);
-		return ResponseEntity.ok(user);
-	}
-	
+    private static final Logger LOG = LogManager.getLogger(UserController.class);
+
+    private final UserRepository userRepository;
+
+    private final CartRepository cartRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository, CartRepository cartRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.cartRepository = cartRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.of(userRepository.findById(id));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        User user = new User();
+        user.setUsername(createUserRequest.getUsername());
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        user.setCart(cart);
+        if (createUserRequest.getPassword().length() < 7 ||
+                !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+            LOG.error("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create " + createUserRequest.getUsername());
+            return ResponseEntity.badRequest().build();
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
+    }
+
 }
